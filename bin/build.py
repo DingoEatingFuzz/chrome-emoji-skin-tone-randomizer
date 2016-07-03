69ef5fee5b26e8f1d4b7b405a62cbcde6e5d5a0b
Michael Lange <dingoeatingfuzz@gmail.com>
2016-07-02 20:38:38 -0700

    Pulls walkTree into its own helper file

--- bin/build.py
+++ bin/build.py
@@ -5,12 +5,15 @@ from subprocess import call
 
 call(["./bin/save-emoji-modifier-bases.py"])
 skintones = open('./bin/emoji-support.js', 'r', encoding='utf-8').read()
+walk_tree = open('./bin/walk-tree.js', 'r', encoding='utf-8').read()
 
 def exec_templates(infile):
   with open('./src/%s' % infile, 'r', encoding='utf-8') as incoming:
     source = incoming.read()
     with open('./dist/%s' % infile, 'w', encoding='utf-8') as outgoing:
-      outgoing.write(source.replace("{{skintoneEligible}}", skintones))
+      out = source.replace("{{skintoneEligible}}", skintones)
+      out = out.replace("{{walkTree}}", walk_tree)
+      outgoing.write(out)
 
 def copy(*files):
   for f in files:
@@ -20,3 +23,5 @@ exec_templates('options.js')
 exec_templates('postprocess.js')
 
 copy('manifest.json', 'options.html', 'styles.css')
+
+print("Project built at ./dist")
--- src/postprocess.js
+++ src/postprocess.js
@@ -10,28 +10,4 @@ function randomizeSkintones (el) {
   })
 }
 
-function walkTree (node, mutator) {
-  // Adapted from Cloud to Butt: https://github.com/panicsteve/cloud-to-butt/blob/master/Source/content_script.js
-  let child, next
-
-  if (node.tagName && node.classList && (
-    node.tagName.toLowerCase() === 'input' ||
-    node.tagName.toLowerCase() === 'textarea' ||
-    node.classList.contains('ace_editor')
-  )) { return }
-
-  switch (node.nodeType) {
-    case 1:  // Element
-    case 9:  // Document
-    case 11: // Document fragment
-      child = node.firstChild
-      while (child) {
-        next = child.nextSibling
-        walkTree(child, mutator)
-        child = next
-      }
-      break
-    case 3: // Text node
-      mutator(node)
-  }
-}
+{{walkTree}}
